3a762ea29f3d5aee76bdf3ce376bac4f6dbea53c
Bikash-web702 <20027720@students.koi.edu.au>
2025-08-18 19:39:51 +1000

    code
    simple code from lab

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();  // Reads user input
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
